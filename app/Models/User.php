54e853195a648bb08bdec2228389fcbc39e0a46d
thefeqy <thefeqy@gmail.com>
2024-12-18 02:11:15 +0100

    Define Models & Migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -46,5 +47,14 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the features for the user.
+     * @return HasMany<Feature>
+     */
+    public function features(): HasMany
+    {
+        return $this->hasMany(Feature::class);
     }
 }
